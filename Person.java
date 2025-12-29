4e764c3503d0cef2c9b34aecb474f578ace43b3d
Jakub <jakub.ciomcia2001@gmail.com>
2025-12-29 21:28:49 +0100

    feature-commit-1

--- Person.java
+++ Person.java
@@ -1,13 +1,21 @@
 import java.math.BigDecimal;
 
 public class Person{
+
+    public static final String HEIGHT = "100";
+
     private String name;
     private BigDecimal salary;
+    private String pesel;
 
     public Integer age(){
         return 40;
     }
 
-    public static final String HEIGHT = "100";
+
+    public String whatIsYourHobby(){
+        return "pizza";
+    }
+
 
 }
