1ef8732952cab3403f13a0e2cedc22b3fba490f6
Jakub <jakub.ciomcia2001@gmail.com>
2026-01-02 18:21:24 +0100

    commit-7

--- Dog.java
+++ Dog.java
@@ -1,3 +1,10 @@
 
 public class Dog {
+    private String tam;
+    private String iTu;
+    private String pałeczka;
+    private String pałeczk2;
+    private String pałeczk3;
+    private String tam2;
+    private String iTu2;
 }
