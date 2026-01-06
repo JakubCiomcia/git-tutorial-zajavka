5564d04b82e736550c2ec4a575a65c18764f59a6
Jan Kowalski <jan.kowalski@example.com>
2026-01-06 02:42:06 +0100

    Person 10

--- Person.java
+++ Person.java
@@ -20,6 +20,9 @@ public class Person{
 
     public void hello(){
         System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
 
 
     }
