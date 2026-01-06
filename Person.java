b46dc5719876404b766c1d8dc014d1d899da66c5
Jakub <jakub.ciomcia2001@gmail.com>
2026-01-06 02:30:39 +0100

    Person 3

--- Person.java
+++ Person.java
@@ -21,12 +21,6 @@ public class Person{
     public void hello(){
         System.out.println("Hello!");
         System.out.println("Hello!");
-        System.out.println("Hello!");
-        System.out.println("Hello!");
-        System.out.println("Hello!");
-        System.out.println("Hello!");
-        System.out.println("Hello!");
-        System.out.println("Hello!");
 ;
 
     }
